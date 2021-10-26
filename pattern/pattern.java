72c0e0b67958c03c7da4b889ac1b33af508c27da
Sandeep Sokle <sandeepsokle12@gmail.com>
2021-10-26 23:26:49 +0530

    up

--- pattern/pattern.java
+++ pattern/pattern.java
@@ -42,6 +42,11 @@ public class pattern {
         }
     }
 
+    // Home work
+    // public static void pattern4(int n){
+
+    // }
+
     public static void pattern5(int n) {
         if (n % 2 == 0) {
             System.out.println("Please Enter Odd number");
@@ -66,6 +71,67 @@ public class pattern {
         }
     }
 
+    // Home work
+    public static void pattern6(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Please Enter Odd number");
+            return;
+        }
+        int nst = n / 2 + 1, nsp = 1;
+        for (int i = 0; i < n; i++) {
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
+            }
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print(" \t");
+            }
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+            if (i >= n / 2) {
+                nst++;
+                nsp -= 2;
+                continue;
+            }
+            nst--;
+            nsp += 2;
+        }
+    }
+
+    // Home work
+    // public static void pattern7(int n){
+
+    // }
+
+    // Home work
+    // public static void pattern8(int n){
+
+    // }
+
+    // Home work
+    // public static void pattern9(int n){
+
+    // }
+
+    // Home work
+    public static void pattern10(int n) {
+        int mid = n /2;
+
+        for (int i = 0; i <= n; i++) {
+            int val = i % (mid + 1);
+            for (int j = 0; j < n; j++) {
+                if( j == mid - val || j == mid + val && j <){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] argv) {
         Scanner scn = new Scanner(System.in);
 
@@ -74,7 +140,9 @@ public class pattern {
         // pattern1(n);
         // pattern2(n);
         // pattern3(n);
-        pattern5(n);
+        // pattern5(n);
+        // pattern6(n);
+        pattern10(n);
 
     }
 
