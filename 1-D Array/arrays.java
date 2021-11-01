73430a7724f044f74082e9fdedcbee35d5f8aa52
Sandeep Sokle <sandeepsokle12@gmail.com>
2021-11-02 00:12:50 +0530

    up

---
+++ 1-D Array/arrays.java
@@ -0,0 +1,69 @@
+
+import java.util.*;
+
+public class arrays {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void input(int[] arr, int size) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = scn.nextInt();
+        }
+    }
+
+    public static void output(int[] arr) {
+        // int size = arr.length;
+
+        // simple loop
+        // for(int i = 0 ; i < size; i++){
+        // System.out.print(arr[i] + " ");
+        // }
+
+        // for each loop
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+    }
+
+
+    public static int[] inputReturnArray(int n){
+
+       
+        int[] arr2= new int[n];
+        for (int i = 0; i < n; i++) {
+            arr2[i] = scn.nextInt();
+        }
+
+        return arr2;
+
+    }
+
+
+    public static int[] inverse(int[] arr, int size){
+        int[] ans = new int[size];
+        for(int i = 0 ; i< size ; i++){
+            ans[arr[i]] = i;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        // // input size
+        int n = scn.nextInt();
+        // // initialaze array
+
+        int[] arr = new int[n];
+        // System.out.println(arr[4]);
+
+        // // input()
+        input(arr, n);
+        // // output()
+
+
+
+        // function, return a array
+        // int[] arr = inputReturnArray(n);
+
+        output(inverse(arr, n));
+    }
+}
